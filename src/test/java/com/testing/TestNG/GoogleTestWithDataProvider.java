a77cf8b7bdaa420ef3d5d5d665dd411f7ae9046f
Bhanu P <bhannushankar422@gmail.com>
2020-08-03 22:46:23 -0500

    Added test suite

--- src/test/java/com/testing/TestNG/GoogleTestWithDataProvider.java
+++ src/test/java/com/testing/TestNG/GoogleTestWithDataProvider.java
@@ -9,8 +9,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 
-public class GoogleTest {
+public class GoogleTestWithDataProvider {
 	WebDriver driver;
 
 	@BeforeMethod
@@ -19,23 +20,19 @@ public class GoogleTest {
 		driver = new ChromeDriver();
 	}
 
-	@Test(description = "Test for selenium keyword")
-	public void testSeleniumSearch() {
-		testKeywordSearch("selenium");
-	}
-	
-	@Test(description = "Test for java keyword")
-	public void testJavaSearch() {
-		testKeywordSearch("java");
-	}
-
-	private void testKeywordSearch(String keyword) {
+	@Test(dataProvider = "dp", description="Testing with DataProvider")
+	public void f(Integer n, String s) {
 		driver.get("https://www.google.com");
-		  driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys(keyword);
+		  driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys(s);
 		  driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys(Keys.ENTER);
 		  String results = driver.findElement(By.xpath("//*[@id=\"result-stats\"]")).getText();
 		  System.out.println(results);
 		  assertTrue(results.contains("results"), "Found -> " + results);
 		  driver.close();
+	}
+
+	@DataProvider
+	public Object[][] dp() {
+		return new Object[][] { new Object[] { 1, "selenium" }, new Object[] { 2, "java" }, };
 	}
 }
